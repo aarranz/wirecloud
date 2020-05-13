16102b662398c4f5fd403b6738dc670b6964e2a8
Álvaro Arranz <aarranz@ficodes.com>
2020-05-13 12:13:06 +0200

    Move SidebarLayout to use ES6 classes

--- src/wirecloud/platform/static/js/wirecloud/ui/SidebarLayout.js
+++ src/wirecloud/platform/static/js/wirecloud/ui/SidebarLayout.js
@@ -31,111 +31,111 @@
         "left": "right"
     };
 
-    var SidebarLayout = function SidebarLayout(dragboard, options) {
-        options = utils.merge({
-            position: "left"
-        }, options);
+    Wirecloud.ui.SidebarLayout = class SidebarLayout extends Wirecloud.ui.SmartColumnLayout {
 
-        Wirecloud.ui.SmartColumnLayout.call(
-            this,
-            dragboard,
-            1,
-            12,
-            4,
-            3,
-            12
-        );
+        constructor(dragboard, options) {
+            super(
+                dragboard,
+                1,
+                12,
+                4,
+                3,
+                12
+            );
 
-        privates.set(this, {
-            active: false
-        });
+            options = utils.merge({
+                position: "left"
+            }, options);
 
-        Object.defineProperties(this, {
-            position: {
-                value: options.position
-            },
-            active: {
-                get: on_active_get,
-                set: on_active_set
+            privates.set(this, {
+                active: false
+            });
+
+            Object.defineProperties(this, {
+                position: {
+                    value: options.position
+                },
+                active: {
+                    get: on_active_get,
+                    set: on_active_set
+                }
+            });
+
+            this.handle = document.createElement("div");
+            this.handleicon = document.createElement("i");
+            this.handleicon.className = "fas fa-caret-" + OPPOSITE[this.position];
+            this.handle.appendChild(this.handleicon);
+            this.handle.addEventListener("click", () => {
+                this.active = !this.active;
+            });
+            this.handle.className = "wc-sidebar-" + this.position + "-handle hidden";
+        }
+
+        addWidget(widget, affectsDragboard) {
+            const result = Wirecloud.ui.SmartColumnLayout.prototype.addWidget.call(this, widget, affectsDragboard);
+
+            if (this.handle.classList.contains("hidden")) {
+                this.handle.classList.remove("hidden");
             }
-        });
+            if (this.initialized) {
+                this.matrix[0][0].wrapperElement.appendChild(this.handle);
+            }
 
-        this.handle = document.createElement("div");
-        this.handleicon = document.createElement("i");
-        this.handleicon.className = "fas fa-caret-" + OPPOSITE[this.position];
-        this.handle.appendChild(this.handleicon);
-        this.handle.addEventListener("click", () => {
-            this.active = !this.active;
-        });
-        this.handle.className = "wc-sidebar-" + this.position + "-handle hidden";
-    };
-    utils.inherit(SidebarLayout, Wirecloud.ui.SmartColumnLayout);
-
-    SidebarLayout.prototype.addWidget = function addWidget(widget, affectsDragboard) {
-        const result = Wirecloud.ui.SmartColumnLayout.prototype.addWidget.call(this, widget, affectsDragboard);
-
-        if (this.handle.classList.contains("hidden")) {
-            this.handle.classList.remove("hidden");
-        }
-        if (this.initialized) {
-            this.matrix[0][0].wrapperElement.appendChild(this.handle);
+            return result;
         }
 
-        return result;
-    };
+        removeWidget(widget, affectsDragboard) {
+            const result = Wirecloud.ui.SmartColumnLayout.prototype.removeWidget.call(this, widget, affectsDragboard);
 
-    SidebarLayout.prototype.removeWidget = function removeWidget(widget, affectsDragboard) {
-        const result = Wirecloud.ui.SmartColumnLayout.prototype.removeWidget.call(this, widget, affectsDragboard);
-
-        if (Object.keys(this.widgets).length === 0) {
-            this.handle.classList.add("hidden");
-            this.handle.remove();
-        } else {
-            this.matrix[0][0].wrapperElement.appendChild(this.handle);
-        }
-        return result;
-    };
-
-    SidebarLayout.prototype.adaptColumnOffset = function adaptColumnOffset(value) {
-        return new Wirecloud.ui.MultiValuedSize(0, 0);
-    };
-
-    SidebarLayout.prototype.adaptWidth = function adaptWidth(size) {
-        return new Wirecloud.ui.MultiValuedSize(this.getWidth(), 1);
-    };
-
-    SidebarLayout.prototype.getWidth = function getWidth() {
-        return 497;
-    };
-
-    SidebarLayout.prototype.initialize = function initialize() {
-        let modified = Wirecloud.ui.SmartColumnLayout.prototype.initialize.call(this);
-        if (this.matrix[0][0] != null) {
-            this.matrix[0][0].wrapperElement.appendChild(this.handle);
-        }
-        return modified;
-    };
-
-    SidebarLayout.prototype.updatePosition = function updatePosition(widget, element) {
-        var offset;
-        if (!this.active) {
-            offset = -this.getWidth() - this.leftMargin + this.dragboard.leftMargin;
-        } else {
-            offset = 0;
+            if (Object.keys(this.widgets).length === 0) {
+                this.handle.classList.add("hidden");
+                this.handle.remove();
+            } else {
+                this.matrix[0][0].wrapperElement.appendChild(this.handle);
+            }
+            return result;
         }
 
-        element.style.top = this.getRowOffset(widget.position) + "px";
-        element.style.bottom = "";
-        if (this.position === "left") {
-            element.style.left = offset + "px";
-            element.style.right = "";
-        } else {
-            element.style.right = offset + "px";
-            element.style.left = "";
+        adaptColumnOffset(value) {
+            return new Wirecloud.ui.MultiValuedSize(0, 0);
         }
-    };
 
-    Wirecloud.ui.SidebarLayout = SidebarLayout;
+        adaptWidth(size) {
+            return new Wirecloud.ui.MultiValuedSize(this.getWidth(), 1);
+        }
+
+        getWidth() {
+            return 497;
+        }
+
+        initialize() {
+            let modified = Wirecloud.ui.SmartColumnLayout.prototype.initialize.call(this);
+            if (this.matrix[0][0] != null) {
+                this.matrix[0][0].wrapperElement.appendChild(this.handle);
+            }
+            return modified;
+        }
+
+        updatePosition(widget, element) {
+            let offset;
+            if (!this.active) {
+                offset = -this.getWidth() - this.leftMargin + this.dragboard.leftMargin;
+            } else {
+                offset = 0;
+            }
+
+            element.style.top = this.getRowOffset(widget.position) + "px";
+            element.style.bottom = "";
+            if (this.position === "left") {
+                element.style.left = offset + "px";
+                element.style.right = "";
+            } else {
+                element.style.right = offset + "px";
+                element.style.left = "";
+            }
+        }
+
+    }
 
     const privates = new WeakMap();
 
